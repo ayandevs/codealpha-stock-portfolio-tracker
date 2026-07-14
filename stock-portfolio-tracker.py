a6f44e9abc79387245f6cd51754fd228dfa05435
ayandevs <nadeemayan769@gmail.com>
2026-07-15 00:30:53 +0500

    enhanced code

--- stock-portfolio-tracker.py
+++ stock-portfolio-tracker.py
@@ -6,7 +6,8 @@ stock_prices = {
 }
 
 total_amount = 0
-stock_details = []
+stock_details = {}
+quantity_details = 0
 
 print("="*35)
 print("Welcome to Stock Portfolio Tracker")
@@ -18,10 +19,16 @@ while True :
     if stock not in stock_prices :
         print("Sorry, Stock not found!")
         continue
-    
-    stock_details.append(stock)
 
     quantity = int(input("Enter the quantity you have : "))
+
+    if quantity < 0 :
+        print("Invalid quantity! Please enter a positive number.")
+        continue
+    
+    stock_details.update({stock:quantity})
+
+    quantity_details += quantity
 
     total_amount += stock_prices[stock]*quantity
 
@@ -30,12 +37,29 @@ while True :
     if(choice == "no") :
         break
     
+    print("\n")
+
+print("\nYour Stock Portfolio Details : \n")
+
 for i  in stock_details :
     print(f"Stock Name : {i} | Price : {stock_prices[i]}$")
 
+print(f"Your total investment in stocks is : {str(total_amount)}$")
+
 with open("portfolio.txt" , "w") as f:
+
+    f.write("Your Stock Portfolio Details : \n")
+    f.write("\n")
+
     for i  in stock_details :
         f.write(f"Stock Name : {i} | Price : {stock_prices[i]}$\n")
-    f.write(f"Your total investment in stocks is : {str(total_amount)}$")
+    
+    f.write("\n")
 
-print("Your investment detail is stored in portfolio.txt")
+    for i in stock_details :
+        f.write(f"you have \"{stock_details[i]}\" stocks of \"{i}\" \n")
+    
+    f.write(f"\nYour total stocks quantity is : {str(quantity_details)}")
+    f.write(f"\nYour total investment in stocks is : {str(total_amount)}$")
+
+print("\nYour investment detail is stored in portfolio.txt")
